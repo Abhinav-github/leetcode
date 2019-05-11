798db6382f4600d19fa023a789e583f20c523228
Abhinav-github <singh.avi99@gmail.com>
2019-05-10 19:02:15 -0500

    Update TwoSum.py
    Finished

--- TwoSum.py
+++ TwoSum.py
@@ -1,9 +1,14 @@
-/*
-Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-*/
+#Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+#You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
+        hash = {}
+        answer = []
+        for i in range(len(nums)):
+            if nums[i] in hash:
+                answer.append(hash[nums[i]])
+                answer.append(i)
+                return answer
+            hash[target - nums[i]] = i
+        return answer
