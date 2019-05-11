bf8a5a4884526589bdf38349b8857c9714f4e97d
Abhinav-github <singh.avi99@gmail.com>
2019-05-11 15:52:42 -0400

    Update BestTimetoBuyandSellStock.py

--- BestTimetoBuyandSellStock.py
+++ BestTimetoBuyandSellStock.py
@@ -5,4 +5,10 @@
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        
+        max_profit = 0
+        min_price = sys.maxsize
+        for i in range(len(prices)):
+            min_price = min(min_price,prices[i])
+            curr_profit = prices[i] - min_price
+            max_profit = max(max_profit, curr_profit)
+        return max_profit
