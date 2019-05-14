a31a7cebf4e015eba65dd9fe73a1970d04e8e79e
Abhinav-github <singh.avi99@gmail.com>
2019-05-14 16:25:23 -0400

    Update MaximumSubarray.py

--- MaximumSubarray.py
+++ MaximumSubarray.py
@@ -3,4 +3,10 @@
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        
+        current = 0
+        result = nums[0]
+        for i in range(len(nums)):
+            current += nums[i]
+            result = max(current,result)
+            current = max(0,current)
+        return result
