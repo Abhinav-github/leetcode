78a5a21f513593c79ea1f5bfcda16186c042c397
Abhinav-github <singh.avi99@gmail.com>
2019-05-13 20:32:43 -0400

    Update ProductofArrayExceptSelf.py

--- ProductofArrayExceptSelf.py
+++ ProductofArrayExceptSelf.py
@@ -4,4 +4,14 @@
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        
+        answer = []
+        length = len(nums)
+        right_total = 1
+        for i in range(len(nums)):
+            answer.append(right_total)
+            right_total = right_total * nums[i]
+        left_total = 1
+        for i in range(len(nums) - 1, -1,-1):
+            answer[i] = left_total * answer[i]
+            left_total = left_total * nums[i]
+        return answer
