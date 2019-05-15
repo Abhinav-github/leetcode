02b136bb2b76810b0622aaa70511c2261cc8d756
Abhinav-github <singh.avi99@gmail.com>
2019-05-15 13:03:15 -0400

    Create MaximumProductSubarray.py

---
+++ MaximumProductSubarray.py
@@ -0,0 +1,6 @@
+#Given an integer array nums, find the contiguous subarray within an
+#array (containing at least one number) which has the largest product.
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        
