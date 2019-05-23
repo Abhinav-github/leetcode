ef43d1e830c79e69034dcd3cc2c678e53c4cba11
Abhinav-github <singh.avi99@gmail.com>
2019-05-23 16:52:16 -0400

    Update ReverseBits.cpp

--- ReverseBits.cpp
+++ ReverseBits.cpp
@@ -3,6 +3,15 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
+        uint32_t ret = 0;
+        int i;
+        for(i = 0; i < 32; i++) {
+            if( n & 1 ) {
+                ret |=  1 << ( 31 - i );                
+            }
+        n >>= 1;
+        }
         
+        return ret;
     }
 };
