121156f7f304a12676e9b5a34f5bf849a86501e9
Abhinav-github <singh.avi99@gmail.com>
2019-05-23 16:25:02 -0400

    Update CountingBits.py

--- CountingBits.py
+++ CountingBits.py
@@ -4,3 +4,7 @@
 
 class Solution:
     def countBits(self, num: int) -> List[int]:
+        ret = [0]
+        for i in range(1,num+1):
+            ret.append(ret[i>>1] + (i&1))
+        return ret
