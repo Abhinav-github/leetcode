eac8fdd6c72b7018a71842507ee276168f563d71
Abhinav-github <singh.avi99@gmail.com>
2019-05-23 16:30:17 -0400

    Update MissingNumber.py

--- MissingNumber.py
+++ MissingNumber.py
@@ -1,5 +1,14 @@
 #Given an array containing n distinct numbers taken from 
 #0, 1, 2, ..., n, find the one that is missing from the array.
 
+#slow version
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        for i in range(len(nums)):
+            if i != nums[i]:
+                return i
+        return len(nums)
+    
+class Solution:
+def missingNumber(self, nums: List[int]) -> int:
