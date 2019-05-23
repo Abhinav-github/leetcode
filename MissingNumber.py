6229f4e96052a817976c9bc7438eca544d116215
Abhinav-github <singh.avi99@gmail.com>
2019-05-23 16:33:31 -0400

    Update MissingNumber.py

--- MissingNumber.py
+++ MissingNumber.py
@@ -9,6 +9,14 @@ class Solution:
             if i != nums[i]:
                 return i
         return len(nums)
-    
+
+#assuming missing value replaced with next value
 class Solution:
-def missingNumber(self, nums: List[int]) -> int:
+    def missingNumber(self, nums: List[int]) -> int:
+        sum1 = 0
+        expected_sum = 0
+        for i in range(len(nums)):
+            sum1 += nums[i]
+            expected_sum += i
+        sum1 -= len(nums)
+        return expected_sum - sum1
