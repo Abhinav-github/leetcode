0db1667a1edfc828bca50bef683ff26d453e4e0d
Abhinav-github <singh.avi99@gmail.com>
2019-05-24 17:00:00 -0400

    Update ClimbingStairs.py

--- ClimbingStairs.py
+++ ClimbingStairs.py
@@ -3,4 +3,10 @@
 
 class Solution:
     def climbStairs(self, n: int) -> int:
-        
+        a = 1
+        b = 1
+        for i in range(n):
+            c = a + b 
+            a = b
+            b = c
+        return a
