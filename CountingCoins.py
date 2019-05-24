e353d71c98c3c671a5eab3477dd765ddc3293701
Abhinav-github <singh.avi99@gmail.com>
2019-05-24 17:18:55 -0400

    Update CountingCoins.py

--- CountingCoins.py
+++ CountingCoins.py
@@ -5,4 +5,13 @@
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        
+        ret = [amount+1] * (amount+1)
+        ret[0] = 0
+        for i in range(1, amount+1):
+            for c in coins:
+                if i >= c:
+                    ret[i] = min(ret[i], ret[i-c] + 1)
+
+        if ret[amount] == amount+1:
+            return -1
+        return ret[amount]        
