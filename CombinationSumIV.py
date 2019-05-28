0e008b199f0559900e3101d6a222267640d6dbba
Abhinav-github <singh.avi99@gmail.com>
2019-05-27 23:13:02 -0400

    Update CombinationSumIV.py

--- CombinationSumIV.py
+++ CombinationSumIV.py
@@ -4,4 +4,15 @@
 
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        
+        if len(nums) == 0 or target == 0:
+            return 0
+        tar = []
+        for i in range(target +  1):
+            tar.append(0)
+        for i in range(1, target+1):
+            for j in nums:
+                if i == j:
+                    tar[i] += 1
+                elif i - j > 0:
+                    tar[i] += tar[i - j]
+        return tar[target]
