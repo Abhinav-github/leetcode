db6c0abf4271389eb1dfe49a43c53cb981fa1586
Abhinav-github <singh.avi99@gmail.com>
2019-06-03 14:58:20 -0400

    Update DecodeWays.py

--- DecodeWays.py
+++ DecodeWays.py
@@ -8,4 +8,12 @@
 
 class Solution:
     def numDecodings(self, s: str) -> int:
-        
+        if len(s) == 0:
+            return 0
+        ret = [1] + [0] * len(s)
+        for i in range(1, len(s) + 1):
+            if s[i - 1] != '0':
+                ret[i] += ret[i - 1]
+            if i >= 2 and 10 <= int(s[i - 2: i]) <= 26: 
+                ret[i] += ret[i-2]
+        return ret[len(s)]
