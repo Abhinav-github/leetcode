e2f3c929402219d9966472a773d2815a33eeee0c
Abhinav-github <singh.avi99@gmail.com>
2019-06-04 15:18:30 -0400

    Update JumpGame.py

--- JumpGame.py
+++ JumpGame.py
@@ -4,4 +4,8 @@
 
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        
+        final = len(nums) - 1
+        for i in range(len(nums))[::-1]:
+            if i + nums[i] >= final:
+                final = i
+        return not final
