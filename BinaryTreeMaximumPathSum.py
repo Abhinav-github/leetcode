88cb4653bf867b9a2335a8a0ad597b0397cbb084
Abhinav-github <singh.avi99@gmail.com>
2019-06-10 21:10:41 -0400

    Update BinaryTreeMaximumPathSum.py

--- BinaryTreeMaximumPathSum.py
+++ BinaryTreeMaximumPathSum.py
@@ -12,5 +12,18 @@
 #         self.right = None
 
 class Solution:
-    def maxPathSum(self, root: TreeNode) -> int:
-        
+    def maxPathSum(self, root):
+        self.res = -sys.maxsize-1
+        self.oneSideSum(root)
+        return self.res
+    
+    # compute one side maximal sum, 
+    # (root+left children, or root+right children),
+    # root is the included top-most node 
+    def oneSideSum(self, root):
+        if not root:
+            return 0
+        left = max(0, self.oneSideSum(root.left))
+        right = max(0, self.oneSideSum(root.right))
+        self.res = max(self.res, left + right + root.val)
+        return max(left, right) + root.val
