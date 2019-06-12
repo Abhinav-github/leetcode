18caa564c7d13b496df95103e045851a9d88503f
Abhinav-github <singh.avi99@gmail.com>
2019-06-11 21:48:50 -0400

    Create BinaryTreeLevelOrderTraversal.py

---
+++ BinaryTreeLevelOrderTraversal.py
@@ -0,0 +1,13 @@
+#Given a binary tree, return the level order traversal 
+#of its nodes' values. (ie, from left to right, level by level).
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        
