7131dab8e2fc0bb7289767cf0b672b7347ed2397
Abhinav-github <singh.avi99@gmail.com>
2019-06-11 22:53:57 -0400

    Update BinaryTreeLevelOrderTraversal.py

--- BinaryTreeLevelOrderTraversal.py
+++ BinaryTreeLevelOrderTraversal.py
@@ -10,4 +10,19 @@
 
 class Solution:
     def levelOrder(self, root: TreeNode) -> List[List[int]]:
-        
+        ret = []
+        level = []
+        if not root:
+            return ret
+        ret.append((root,0))
+        while ret:
+            node, curr_level = ret.pop(0)
+            if len(level) == curr_level:
+                level.append([node.val])
+            else:
+                level[curr_level].append(node.val)
+            if node.left:
+                ret.append((node.left,curr_level+1))
+            if node.right:
+                ret.append((node.right,curr_level+1))
+        return level
